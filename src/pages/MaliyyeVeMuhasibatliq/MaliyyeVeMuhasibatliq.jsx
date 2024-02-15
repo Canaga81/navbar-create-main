3f5552dfbc4d9ba6491f8f857e2d6e33e9c187a5
Canaga81 <Canagamemmedov@gmail.com>
2024-02-15 16:15:57 +0400

    Maliye Muhasibat Component Finish

--- src/pages/MaliyyeVeMuhasibatliq/MaliyyeVeMuhasibatliq.jsx
+++ src/pages/MaliyyeVeMuhasibatliq/MaliyyeVeMuhasibatliq.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import Maliye_Bg_Image from '../../images/maliyye_bg_image.jpg';
+import './MaliyyeVeMuhasibatliq.css'
 
 const MaliyyeVeMuhasibatliq = () => {
   return (
-    <div>
-        MaliyyeVeMuhasibatliq
+    <div className='maliyeMuhContainer'>
+        <div className='maliyeMuhaConImgText'>
+            <div className="maliyeMuhaConImg">
+                <img className='maliyeMuhaConImage' src={Maliye_Bg_Image} alt="" />
+            </div>
+            <div className='maliyeMuhaTextCon'>
+                <div className='maliyeMuhaTextCon_Top'>
+                    <p className='maliyeMuhaTextCon_Top-p_bold'>Mailiyyə və mühasibat, bir şirkətin effektiv idarəetməsində və uğurlu nailiyyətlərdə əhəmiyyətli bir rol oynayan bölmələrdən biridir.</p>
+                    <p className='maliyeMuhaTextCon_Top-p'>Mailiyyə mühasibatlığı, bir şirkətin maliyyə və əmanət proseslərini idarə etmək, tənzimləmək və nəzarət etmək üçün məsuliyyət daşıyan təşkilat hissəsidir. Bu bölmə, maliyyə hesabatları, bütçələmə, əmanətlər, ödənişlər, qazanc və zərər hesabatları kimi müxtəlif sahələrdə fəaliyyət göstərir.</p>
+                </div>
+                <div className='maliyeMuhaTextCon_Bottom'>
+                    <h3 className='maliyeMuhaTextCon_Bottom-h3'>Mailiyyə mühasibatlığının əsas məqsədləri bunlardır:</h3>
+                    <div className='maliyeMuhaTextCon_Bottom-ul'>
+                        <div className='maliyeMuhaTextCon_Bottom-li'>
+                            <span>Maliyyə Hesabatları </span> - <p> Şirkətin maliyyə vəziyyəti ilə bağlı ətraflı və dəqiqlik hesabatlar hazırlamaq.</p>
+                            
+                        </div>
+
+                        <div className='maliyeMuhaTextCon_Bottom-li'>
+                            <span>Bütçələmə </span> - <p> Gəlirlər və xərclər arasında dəqiqlik təyin etmək və bütçələrin hazırlanması, təhlili və izlənməsinə kömək etmək.</p>
+                            
+                        </div>
+                        <div className='maliyeMuhaTextCon_Bottom-li'>
+                            <span>Əmanətlər </span> - <p> Şirkətin varlıqları və özəllikləri üzrə müəyyən etmələr aparmaq, əmanətlərin idarə olunmasına kömək etmək.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
   )
 }
 
-export default MaliyyeVeMuhasibatliq
+export default MaliyyeVeMuhasibatliq;
